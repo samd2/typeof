e82b21d2fd683476907d1abc9fb32e9fd3fbddfb
sdarwin <samuel.d.darwin@gmail.com>
2021-01-25 16:41:20 +0000

    test

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-set -ex
+# Copyright 2020 Rene Rivera, Sam Darwin
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+set -e
 export TRAVIS_BUILD_DIR=$(pwd)
+export DRONE_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=$DRONE_BRANCH
-export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
 export VCS_COMMIT_ID=$DRONE_COMMIT
 export GIT_COMMIT=$DRONE_COMMIT
-export DRONE_CURRENT_BUILD_DIR=$(pwd)
+export REPO_NAME=$DRONE_REPO
 export PATH=~/.local/bin:/usr/local/bin:$PATH
 
-echo '==================================> BEFORE_INSTALL'
-
-. .drone/before-install.sh
+if [ "$DRONE_JOB_BUILDTYPE" == "boost" ]; then
 
 echo '==================================> INSTALL'
 
@@ -27,15 +29,9 @@ python tools/boostdep/depinst/depinst.py typeof
 ./bootstrap.sh
 ./b2 headers
 
-echo '==================================> BEFORE_SCRIPT'
-
-. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
-
 echo '==================================> SCRIPT'
 
 echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
 ./b2 libs/typeof/test toolset=$TOOLSET cxxstd=$CXXSTD
 
-echo '==================================> AFTER_SUCCESS'
-
-. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
+fi
